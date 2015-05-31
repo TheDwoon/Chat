f93604cd95eb9bd4c33147778044452fdd380c4f
Sigmar Walter <sigmar-walter@web.de>
2015-05-31 19:11:03 +0200

    Added maven projects

---
+++ client/src/main/java/edu/kit/fsmi/schnupperstudium/chat/client/Client.java
@@ -0,0 +1,9 @@
+package edu.kit.fsmi.schnupperstudium.chat.client;
+
+public class Client {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
---
+++ server/src/main/java/edu/kit/fsmi/schnupperstudium/chat/server/Server.java
@@ -0,0 +1,9 @@
+package edu.kit.fsmi.schnupperstudium.chat.server;
+
+public class Server {
+
+	public static void main(final String[] args) {
+
+	}
+
+}
