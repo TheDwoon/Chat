a2a83f9e51433212b260950c7be009509affaeeb
Daniel Wieland <d.wieland@arcor.de>
2015-07-08 01:57:48 +0200

    tried to get RMI working. (didn't though)

--- common/src/main/java/edu/kit/fsmi/schnupperstudium/chat/common/network/RMIAdapter.java
+++ common/src/main/java/edu/kit/fsmi/schnupperstudium/chat/common/network/RMIAdapter.java
@@ -2,87 +2,89 @@ package edu.kit.fsmi.schnupperstudium.chat.common.network;
 
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.HashMap;
+import java.util.List;
 import java.util.function.Function;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-@Deprecated
 public final class RMIAdapter implements PacketExecutor {
-	private static final HashMap<Class<?>, Function<DataInputStream, Object>> parsers = new HashMap<>();
+	private static final HashMap<Class<?>, BinaryDeserializer<?>> PARSERS = new HashMap<>();
 	private static final Logger LOG = LogManager.getLogger();
 	
 	static {
-		parsers.put(Integer.class, (input) -> { 
-			try {
-				return input.readInt();
-			} catch (IOException e) {
-				throw new RuntimeException("failed to parse integer");
-			} 
-		});		
-		parsers.put(int.class, parsers.get(Integer.class));
-		
-		parsers.put(Float.class, (input) -> {
-			try {
-				return input.readFloat();
-			} catch (IOException e) {
-				throw new RuntimeException("failed to parse float");
-			}
+		addParser(int.class, (input) -> {
+			return input.readInt();
 		});
-		parsers.put(float.class, parsers.get(Float.class));
 		
-		parsers.put(Double.class, (input) -> {
-			try {
-				return input.readDouble();
-			} catch (IOException e) {
-				throw new RuntimeException("failed to parse double");
-			}
+		addParser(float.class, (input) -> {
+			return input.readFloat();
+		}); 
+		
+		addParser(double.class, (input) -> {
+			return input.readDouble();
 		});
-		parsers.put(double.class, parsers.get(Double.class));
 		
-		parsers.put(Boolean.class, (input) -> {
-			try {
-				return input.readBoolean();				
-			} catch (IOException e) {
-				throw new RuntimeException("failed to parse boolean");
-			}
+		addParser(boolean.class, (input) -> {
+			return input.readBoolean();
 		});
-		parsers.put(boolean.class, parsers.get(Boolean.class));
 		
-		parsers.put(String.class, (input) -> {
-			try {
-				return input.readUTF();				
-			} catch (IOException e) {
-				throw new RuntimeException("failed to parse string");
-			}
+		addParser(String.class, (input) -> {
+			return input.readUTF();
 		});
 	}
 	
+	private final Object target;
 	private final Method method;
 	private final Class<?>[] parameterTypes;
 	
-	public RMIAdapter(Method method) {
+	public RMIAdapter(Method method, Object target) {
+		this.target = target;
 		this.method = method;
-		this.parameterTypes = method.getParameterTypes();
-		
-		if (parameterTypes.length < 1 || !NetworkChannel.class.isAssignableFrom(parameterTypes[0])) {
-			throw new IllegalArgumentException("The first parameter of a RMI Method must be the channel!");
-		}			
+		this.parameterTypes = method.getParameterTypes();		
 	}
 
 	@Override
 	public boolean executePacket(NetworkChannel channel, Packet packet) {
-		Object[] parameters = new Object[parameterTypes.length];		
-		parameters[0] = channel;
+		Object[] parameters = new Object[parameterTypes.length];	
+		int i = 0;
+		if (NetworkChannel.class.isAssignableFrom(parameterTypes[0])) {
+			parameters[i++] = channel;
+		}
 		
 		DataInputStream input = packet.getInputStream();
-		for (int i = 1; i < parameters.length; i++) {
+		while (i < parameters.length) {
+			BinaryDeserializer<?> deserializer = PARSERS.get(parameterTypes[i]);
+			try {
+				deserializer.deserialize(input);
+			} catch (IOException e) {
+				LOG.error("Caught exception during RMI decoding: ", e);
+				return false;
+			}
 			
+			i++;
 		}
+		
+		try {
+			input.close();
+		} catch (IOException e) {
+			LOG.catching(e);			
+		}
+		
+		try {
+			method.invoke(target, parameters);
+		} catch (Exception e) {
+			LOG.catching(e);
+			return false;
+		} 
 		
 		return true;
 	}
 
+	public static <T> void addParser(Class<T> clazz, BinaryDeserializer<T> deserializer) {
+		PARSERS.put(clazz, deserializer);
+	}
 }
---
+++ common/src/test/java/edu/kit/fsmi/schnupperstudium/chat/common/network/RMITest.java
@@ -0,0 +1,78 @@
+package edu.kit.fsmi.schnupperstudium.chat.common.network;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public final class RMITest {
+	private static final int PORT = 6184;
+	
+	private volatile boolean called = false;
+	
+	private volatile Network network;
+	private volatile NetworkChannel client;
+	private volatile NetworkChannel server;
+	
+	@Before
+	public void setUp() throws IOException, InterruptedException {
+		network = new Network((channel) -> {			
+			try {
+				ExecutorSet executors = new ExecutorSet();
+				executors.addExecutor(1, new RMIAdapter(getClass().getMethod("voidCall"), this));
+				executors.addExecutor(2, new RMIAdapter(getClass().getMethod("simpleCall", int.class), this));
+				
+				channel.addExecutor(executors);
+			} catch (Exception e) {				
+				e.printStackTrace();	
+			}			
+			
+			server = channel;
+		}, PORT);
+		
+		client = new NetworkChannel(new Socket("127.0.0.1", PORT));
+		
+		while (server == null) {
+			Thread.sleep(1);
+		}
+	}
+
+	@Test (timeout = 1000)
+	public void voidCheck() throws InterruptedException {
+		Packet packet = new Packet(1, new byte[0]);
+		client.sendPacket(packet);
+		
+		while (!called) {
+			Thread.sleep(1);
+		}
+		
+		assertTrue(called);
+	}
+	
+	@Test (timeout = 1000)
+	public void simpleCheck() throws InterruptedException {
+		
+		while (!called) {
+			Thread.sleep(1);
+		}
+		
+		assertTrue(called);
+	}
+	
+	@After
+	public void tearDown() throws IOException {
+		network.close();
+	}
+	
+	public void voidCall() {
+		called = true;
+	}
+	
+	public void simpleCall(int magicValue) {
+		called = true;
+	}
+}
